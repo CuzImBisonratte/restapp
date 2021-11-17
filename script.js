de66fb2a296fc1304f0eef63db8fb421cc2fede0
CuzImBisonratte <CuzImBisonratte@gmail.com>
2021-11-17 21:58:34 +0100

    added link extraction

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ if (screenWidth < 768) {
 } else {
     document.body.style.width = "768px";
 }
+
+restaurant_field = document.getElementById("restaurant");
+table_field = document.getElementById("table");
+
+windowLocationSub = location.search.substring(1);
+restaurantRaw = windowLocationSub.split("&")[0];
+restaurant = restaurantRaw.split("=")[1];
+tableRaw = windowLocationSub.split("&")[1];
+table = tableRaw.split("=")[1];
+
+restaurant_field.value = restaurant;
+table_field.value = table;
+
+function open_settings() {
+    document.getElementById("menu").style.display = "block";
+}
+
+function open_help() {
+    document.getElementById("help").style.display = "block";
+}
+
+function close_settings() {
+    document.getElementById("menu").style.display = "none";
+}
+
+function close_help() {
+    document.getElementById("help").style.display = "none";
+}
